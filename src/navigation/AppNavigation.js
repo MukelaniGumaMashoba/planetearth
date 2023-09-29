0e70176da6be3692f21f0e51ed0f6ea2fdf9701f
Mukelani Guma Mashoba <mukelanilastborn@gmail.com>
2023-09-29 08:40:52 +0200

    changes

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,18 +1,60 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import Dashboard from '../screens/Dashboard';
 import Game from '../screens/Game.js';
 import { Settings } from '../screens/Settings';
 import AccountScreen from '../screens/AccountScreen';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 const Tab = createMaterialBottomTabNavigator();
 
 export function AppNavigation() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={Dashboard} />
-            <Tab.Screen name="Game" component={Game} />
-            <Tab.Screen name="Account" component={AccountScreen} />
-            <Tab.Screen name="Settings" component={Settings} />
+        <Tab.Navigator
+            initialRouteName="Home"
+            shifting={true}
+            barStyle={{ backgroundColor: '#ffffff' }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={Dashboard}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color }) => (
+                        <Icon name="home" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Game"
+                component={Game}
+                options={{
+                    tabBarLabel: 'Game',
+                    tabBarIcon: ({ color }) => (
+                        <Icon name="game-controller" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Account"
+                component={AccountScreen}
+                options={{
+                    tabBarLabel: 'Account',
+                    tabBarIcon: ({ color }) => (
+                        <Icon name="person" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Settings"
+                component={Settings}
+                options={{
+                    tabBarLabel: 'Settings',
+                    tabBarIcon: ({ color }) => (
+                        <Icon name="settings" color={color} size={26} />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 }
