21cedb0a4e8e21cc5af1a7567a86ed12f52423cf
Mukelani Guma Mashoba <mukelanilastborn@gmail.com>
2023-09-29 08:38:15 +0200

    changes

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,34 +1,53 @@
 import React from 'react';
-import { Image, StyleSheet, View, TouchableOpacity } from 'react-native';
+import { Text, Image, StyleSheet, View, TouchableOpacity } from 'react-native';
 
-export default function Menu( { navigation } ) {
+export default function Menu({ navigation }) {
     return (
-        <View>
-            <TouchableOpacity onPress={()=>navigation.navigate("Calculator")}>
-                <Image source={require('../assets/calculator.png')} style={styles.image} /></TouchableOpacity>
-            
-            <TouchableOpacity onPress={()=>navigation.navigate("Game")}>
-                <Image source={require('../assets/game-console.png')} style={styles.image} />
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.item} onPress={() => navigation.navigate("Calculator")}>
+                <Image source={require('../assets/calculator.png')} style={styles.image} />
+                <Text style={styles.text}>Calculator</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={()=>navigation.navigate("CostSaving")}>
-                <Image source={require('../assets/piggy-bank.png')} style={styles.image} />
+            <TouchableOpacity style={styles.item} onPress={() => navigation.navigate("Game")}>
+                <Image source={require('../assets/game-console.png')} style={styles.image} />
+                <Text style={styles.text}>Game</Text>
             </TouchableOpacity>
-            
-            <TouchableOpacity onPress={()=>navigation.navigate("Settings")}> 
+
+            <TouchableOpacity style={styles.item} onPress={() => navigation.navigate("CostSaving")}>
+                <Image source={require('../assets/piggy-bank.png')} style={styles.image} />
+                <Text style={styles.text}>Save</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.item} onPress={() => navigation.navigate("Settings")}>
                 <Image source={require('../assets/settings.png')} style={styles.image} />
+                <Text style={styles.text}>Settings</Text>
             </TouchableOpacity>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    image: {
-        width: 80,
-        height: 80,
-        marginBottom: 30,
-        marginTop: 10,
-        justifyContent: 'center',
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
         alignItems: 'center',
+        padding: 20,
+    },
+    item: {
+        alignItems: 'center',
+    },
+    image: {
+        width: 50,
+        height: 50,
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: 'lightgreen',
+        borderRadius: 12,
+    },
+    text: {
+        marginTop: 5,
+        fontSize: 12,
+        textAlign: 'center',
     },
 });
--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,35 +1,72 @@
 import React from 'react';
 import Menu from '../components/menu';
 import { Text, View, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { ScrollView } from 'react-native';
 
 export default function Dashboard({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeText}>Welcome 💫</Text>
-      <Text style={styles.loggedInText}>
-        Congratulations, you are logged in.
-      </Text>
-      <Menu /> 
+
+      <Text style={styles.welcomeText}>PlanetPulse</Text>
+      <Text style={styles.txt}>Know What Happening Around You All The Time.!</Text>
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        <TouchableOpacity style={styles.top}>
+          <Text>Globalization</Text>
+          <Text>Explore more about Globalization in this application and find ways to save...</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.top}>
+          <Text>Globalization</Text>
+          <Text>Explore more about Globalization in this application and find ways to save...</Text>
+        </TouchableOpacity>
+      </ScrollView>
+
+      <View>
+        <Text style={styles.menu}>Menu</Text>
+        <Menu />
+      </View>
+
+      <View>
+        <Text style={styles.menu}>Recommended</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#f0f0f0',
+    margin: 10,
   },
   welcomeText: {
     fontSize: 24,
     fontWeight: 'bold',
     marginVertical: 20,
     color: '#333',
+    textAlign: 'center',
   },
   loggedInText: {
     fontSize: 18,
     textAlign: 'center',
     color: '#666',
+  },
+  top: {
+    borderRadius: 12,
+    backgroundColor: 'purple',
+    width: 240,
+    height: 130,
+    marginBottom: 23,
+    padding: 10,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  menu: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  txt: {
+    fontSize : 20 ,
+    fontWeight: 'bold',
+    marginBottom: 5,
   },
 });
