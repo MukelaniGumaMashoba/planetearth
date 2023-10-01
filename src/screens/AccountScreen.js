ddf227af5aff9a4e7d95087e3100dfc3e9489111
Mukelani Guma Mashoba <mukelanilastborn@gmail.com>
2023-10-01 13:40:47 +0200

    changes

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../../App';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import { Text, View, Button, StyleSheet, Image, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Score from '../components/Score';
 
@@ -13,10 +13,27 @@ const AccountScreen = ({ navigation }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>AccountScreen</Text>
-      <Text>{user.email}</Text>
-      <Score />
-      <Button title="Logout" onPress={handleLogout} />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.profileContainer}>
+          <Image
+            style={styles.profileImage}
+            source={require('../../src/assets/globe.png')}
+          />
+        </View>
+        <Text style={styles.title}>Welcome, {user.name}!</Text>
+        <Text style={styles.subtitle}>Email: {user.email}</Text>
+        <View style={styles.detailsContainer}>
+          <Text style={styles.detailLabel}>Age:</Text>
+          <Text style={styles.detailText}>{user.age}</Text>
+        </View>
+        <View style={styles.detailsContainer}>
+          <Text style={styles.detailLabel}>Location:</Text>
+          <Text style={styles.detailText}>{user.location}</Text>
+        </View>
+        <Score />
+        <Button title="Edit Profile" onPress={() => navigation.navigate('EditProfile')} />
+        <Button title="Logout" onPress={handleLogout} />
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -26,14 +43,41 @@ export default AccountScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  scrollContainer: {
+    padding: 20,
     alignItems: 'center',
+  },
+  profileContainer: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    overflow: 'hidden',
+    marginBottom: 20,
+  },
+  profileImage: {
+    width: '100%',
+    height: '100%',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
     marginBottom: 20,
   },
+  detailsContainer: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  detailLabel: {
+    fontWeight: 'bold',
+    marginRight: 5,
+  },
+  detailText: {
+    fontSize: 16,
+  },
 });
-
-
