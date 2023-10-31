83efa55dbe336b5293688552f2a0ddf911007d7a
Mukelani Guma Mashoba <mukelanilastborn@gmail.com>
2023-10-31 02:57:50 +0200

    Add company badge

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,42 +1,127 @@
-import React, { useContext } from 'react';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { Text, View, Button, StyleSheet, Image, ScrollView, TouchableOpacity } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { db } from '../../firebase';
 import Score from '../components/Score';
 import { UserContext } from '../../userCtxt';
+import { Box, Progress, Text } from 'native-base';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useContext, useEffect, useState } from 'react';
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { View, StyleSheet, Image, ScrollView, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+
 
 const AccountScreen = ({ navigation }) => {
   const { user, doLogout } = useContext(UserContext);
 
+  const [company, setCompany] = useState(null)
+
+  useEffect(() => {
+    const q = query(collection(db, "companies"), where("user", "==", user.uid));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setCompany(null)
+      querySnapshot.forEach((doc) => {
+        setCompany({ ...doc.data(), id: doc.id })
+      });
+    })
+    return () => unsubscribe()
+  }, [])
+
   const handleLogout = () => {
     doLogout();
   };
+  const getTrophyMessage = (index) => {
+    if (index >= 80) {
+      return 'Congratulations! You are the Eco Champion!';
+    } else if (index > 59) {
+      return 'Great job! You are a Carbon Conqueror!';
+    } else if (index > 20) {
+      return 'Well done! You are an Eco Enthusiast!';
+    } else {
+      return `Keep it up! You are making a positive impact!`;
+    }
+  };
 
   return (
+
     <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <View style={styles.account}>
-          <View style={styles.profileContainer}>
-            <Image
-              style={styles.profileImage}
-              source={require('../../src/assets/globe.png')}
-            />
+      <KeyboardAvoidingView>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          <View style={styles.account}>
+            <View style={styles.profileContainer}>
+              <Image
+                style={styles.profileImage}
+                source={require('../../src/assets/globe.png')}
+              />
+            </View>
+
+            <Text style={styles.title}>Welcome, {user.name}!</Text>
+            <Text style={styles.subtitle}>Email: {user.email}</Text>
           </View>
 
-          <Text style={styles.title}>Welcome, {user.name}!</Text>
-          <Text style={styles.subtitle}>Email: {user.email}</Text>
-        </View>
-    
-        <Score />
-        <View style={styles.container2}>
-          <TouchableOpacity onPress={() => navigation.navigate('EditProfile')} style={styles.button}>
-            <Text style={styles.buttonText}>Edit Profile</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={handleLogout} style={[styles.button, styles.logoutButton]}>
-            <Text style={styles.buttonText}>Logout</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+          <Score />
+
+          {company ?
+            <TouchableOpacity onPress={() => {
+              navigation.navigate('Company', company)
+            }} >
+              <Box
+                bg="white"
+                p={4}
+                borderRadius={8}
+                shadow={2}
+                mb={3}
+                maxWidth="100%"
+              >
+                <Text style={{ fontSize: 20, fontWeight: 'bold' }} mb={2}>
+                  {company.name}
+                </Text>
+                <Text fontSize={16} mb={2}>
+                  Carbon Emissions: {company.emissions} tons
+                </Text>
+                <Progress
+                  value={(company.goal - company.emissions) / company.goal * 100}
+                  size="sm"
+                  colorScheme={(company.goal - company.emissions) / company.goal * 100
+                    < 20 ? "red" : (company.goal - company.emissions) / company.goal * 100 < 60 ? "blue" : "green"
+                  }
+                  mb={3}
+                  mt={3}
+                />
+                <Text fontSize={14} fontWeight="bold" mb={2}>
+                  {getTrophyMessage((company.goal - company.emissions) / company.goal * 100)}
+                </Text>
+              </Box>
+            </TouchableOpacity>
+            :
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('Company', {})
+              }} >
+              <Box
+                bg="white"
+                p={4}
+                borderRadius={8}
+                shadow={2}
+                mb={3}
+                maxWidth="100%"
+              >
+                <Text
+                  style={{ fontSize: 16, fontWeight: '400' }}
+                  p={4}>
+                  Click To Add Company
+                </Text>
+              </Box>
+            </TouchableOpacity>}
+
+
+          <View style={styles.container2}>
+            <TouchableOpacity onPress={() => navigation.navigate('EditProfile')} style={styles.button}>
+              <Text style={styles.buttonText}>Edit Profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleLogout} style={[styles.button, styles.logoutButton]}>
+              <Text style={styles.buttonText}>Logout</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
