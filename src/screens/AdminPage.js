a8387d923e8379012e93dd6d78e95a72bed3522e
KingGradi <110394179+KingGradi@users.noreply.github.com>
2023-11-01 14:57:20 +0200

    Admin Page design

--- src/screens/AdminPage.js
+++ src/screens/AdminPage.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
-import { Text, View, FlatList, Button, SafeAreaView, StyleSheet } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { Text, View, FlatList, Button, SafeAreaView, StyleSheet, Image, Alert, TouchableOpacity } from 'react-native';
 import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
 import { db } from '../../firebase';
+import { UserContext } from '../../userCtxt';
 
 export const AdminPage = () => {
     const [users, setUsers] = useState([]);
+
+    const { user, doLogout } = useContext(UserContext);
 
     const fetchUsers = async () => {
         const usersCollectionRef = collection(db, 'companies');
@@ -22,19 +25,55 @@ export const AdminPage = () => {
         fetchUsers();
     };
 
+    const handleLogout = () => {
+        doLogout();
+      };
+
+    const logout = () => {
+        Alert.alert(
+            'Logout',
+            'Are you sure you want to log out?',
+            [
+                {
+                    text: 'Cancel',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Logout',
+                    onPress: () => {
+                        // Perform your logout action here
+                    },
+                },
+            ]
+        );
+    };
+
     return (
         <SafeAreaView style={styles.container}>
-            <Button title="See all users" onPress={fetchUsers} />
-            <FlatList
-                data={users}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                    <View style={styles.userContainer}>
-                        <Text style={styles.emailText}>{item.name}</Text>
-                        <Button title="Delete" onPress={() => deleteUser(item.id)} />
-                    </View>
-                )}
-            />
+            <Image source={require('../assets/LogBack.jpg')} style={styles.backgroundImage} />
+            <View style={styles.centeredView}>
+                <Text style={styles.heading}>Welcome To the Admin Panel</Text>
+                <View style={styles.panel}>
+                    <Button title="See all users" onPress={fetchUsers} color="green" />
+                </View>
+                <FlatList
+                    data={users}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => (
+                        <View style={styles.userContainer}>
+                            <Text style={styles.emailText}>{item.name}</Text>
+                            <Button  title="Delete" onPress={() => deleteUser(item.id)} color="red" />
+                        </View>
+                    )}
+                />
+                
+                <View style={styles.panel}>
+
+                    <TouchableOpacity onPress={logout} style={styles.logout}>
+                        <Text style={styles.buttonText}>LOGOUT</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </SafeAreaView>
     );
 };
@@ -42,7 +81,29 @@ export const AdminPage = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 20,
+    },
+    backgroundImage: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    heading: {
+        fontSize: 24,
+        marginBottom: 20,
+        marginTop: 50,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    panel: {
+        width: 200,
+        marginBottom: 20,
     },
     userContainer: {
         flexDirection: 'row',
@@ -51,9 +112,18 @@ const styles = StyleSheet.create({
         marginVertical: 10,
     },
     emailText: {
-        fontSize: 16,
+        fontSize: 18,
+        marginRight: 20,
     },
-    deleteButton: {
-        marginLeft: 10,
-    }
+    logout: {
+        padding: 10,
+        backgroundColor: 'gray',
+        borderRadius: 5,
+    },
+    buttonText: {
+        padding: 10,
+        fontSize: 16,
+        color: 'white',
+        alignSelf: 'center',
+    },
 });
