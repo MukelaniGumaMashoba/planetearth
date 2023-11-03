900bb6eed44a24764cf4324d897c013ff81b2ceb
Mukelani Guma Mashoba <mukelanilastborn@gmail.com>
2023-11-03 23:54:34 +0200

    move notiffication to AppNav

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -10,6 +10,24 @@ import Calculator from '../screens/Calculator'
 import Gamification from '../screens/Gamification';
 import Company from '../screens/Company';
 import Inbox from '../screens/Inbox'
+import { useState, useEffect, useRef } from 'react';
+import { Platform } from 'react-native';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import Constants from 'expo-constants';
+import { db } from '../../firebase.js';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true,
+    }),
+});
+
 
 const Tab = createMaterialBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -44,6 +62,130 @@ export function AppNavigation() {
 
 
 function StackNavigatorTwo() {
+
+    const [expoPushToken, setExpoPushToken] = useState(null);
+    const [, setNotification] = useState(false);
+
+    const [ready, setReady] = useState(false);
+    const [idss, setIds] = useState([]);
+    const notificationListener = useRef();
+    const responseListener = useRef();
+
+
+    useEffect(() => {
+
+        (async () => {
+            await getSavedNotification();
+        })();
+
+        registerForPushNotificationsAsync().then(token => {
+            setExpoPushToken(token)
+        });
+
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            setNotification(notification);
+        });
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+        });
+
+        return () => {
+            Notifications.removeNotificationSubscription(notificationListener.current);
+            Notifications.removeNotificationSubscription(responseListener.current);
+        };
+    }, []);
+
+    useEffect(() => {
+        let unsubscribe = () => { }
+
+        if (ready) {
+            const q = query(collection(db, "notifications"), orderBy("expire", "desc"));
+            unsubscribe = onSnapshot(q, async (querySnapshot) => {
+                //const noti = [];
+                const ids = [];
+
+                querySnapshot.forEach((doc) => {
+
+                    // noti.push({ ...doc.data(), id: doc.id });
+
+                    if (doc.data().expire >= Date.now() && !idss.includes(doc.id)) {
+                        if (expoPushToken) return schedulePushNotification({ ...doc.data(), to: expoPushToken });
+
+                        schedulePushNotification(doc.data())
+                    }
+
+                    if (!idss.includes(doc.id)) {
+                        ids.push(doc.id);
+                    }
+                });
+
+                setIds((prev) => ([...prev, ...ids]))
+                // setNotifications(noti)
+
+                AsyncStorage.setItem("@notifications", JSON.stringify(idss), (err) => {
+                    // console.error("Saving Eror:", err)
+                })
+
+            });
+        }
+
+        //return () => unsubscribe()
+
+    }, [ready, expoPushToken]);
+
+    const getSavedNotification = async () => {
+        try {
+            const res = await AsyncStorage.getItem("@notifications");
+            // console.log("Saved ids: ", res);
+            if (res) {
+                setIds(JSON.parse(res))
+            }
+            setReady(true)
+        } catch (error) {
+        }
+
+    }
+
+    async function schedulePushNotification(content) {
+        await Notifications.scheduleNotificationAsync({
+            content,
+
+            trigger: { seconds: 1, }
+        });
+    }
+
+    async function registerForPushNotificationsAsync() {
+        let token;
+
+        if (Platform.OS === 'android') {
+            await Notifications.setNotificationChannelAsync('default', {
+                name: 'default',
+                importance: Notifications.AndroidImportance.MAX,
+                vibrationPattern: [0, 250, 250, 250],
+                lightColor: '#FF231F7C',
+            });
+        }
+
+        if (Device.isDevice) {
+            const { status: existingStatus } = await Notifications.getPermissionsAsync();
+            let finalStatus = existingStatus;
+            if (existingStatus !== 'granted') {
+                const { status } = await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            }
+            if (finalStatus !== 'granted') {
+                alert('Failed to get push token for push notification!');
+                return;
+            }
+
+            token = (await Notifications.getExpoPushTokenAsync({ projectId: Constants.expoConfig.extra.eas.projectId, })).data;
+            // console.log(token);
+        } else {
+            alert('Must use physical device for Push Notifications');
+        }
+
+        return token;
+    }
     return (
         <Tab.Navigator
             initialRouteName="Home"
@@ -91,12 +233,6 @@ function StackNavigatorTwo() {
                     ),
                 }}
             />
-
-
-           
-
-
-
         </Tab.Navigator>
     );
 }
