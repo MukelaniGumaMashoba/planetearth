a30c7d721ee318340e617494e3b999dab12f6132
Mukelani Guma Mashoba <mukelanilastborn@gmail.com>
2023-11-03 23:54:05 +0200

    clean up

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import registerNNPushToken, { getNotificationInbox } from 'native-notify';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './src/navigation/AuthNavigation.js';
 import { AppNavigation } from './src/navigation/AppNavigation.js';
@@ -6,7 +5,6 @@ import { LogBox } from 'react-native';
 import { useContext } from "react";
 import UserContextWrapper, { UserContext } from './userCtxt.js';
 import { NativeBaseProvider } from 'native-base';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
   LogBox.ignoreAllLogs();
